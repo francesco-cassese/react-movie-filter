166ff02fde81510ae5519ecf6fbf0010ddf3ac7d
Francesco Cassese <francescocassese9@gmail.com>
2026-05-06 16:00:24 +0200

    feat: reso header un componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import { useState, useEffect } from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <header className="mb-4 bg-dark text-white custom-header">
-      <div className="container-fluid py-3">
-        <div className="row justify-content-center text-center">
-          <div className="col-lg-8">
-            <h1 className="display-1 fw-bold mb-3 tracking-tight">
-              React Movie Filter
-            </h1>
-          </div>
-        </div>
-      </div>
-    </header>
+    <Header />
   );
 }
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header className="mb-4 bg-dark text-white custom-header">
+            <div className="container-fluid py-3">
+                <div className="row justify-content-center text-center">
+                    <div className="col-lg-8">
+                        <h1 className="display-1 fw-bold mb-3 tracking-tight">
+                            React Movie Filter
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Header

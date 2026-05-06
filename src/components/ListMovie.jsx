b0f0929414dcf4d34d8ff243b6583b79101dcfaa
Francesco Cassese <francescocassese9@gmail.com>
2026-05-06 17:44:45 +0200

    refactor: personalizzate ulteriormente le card

--- src/components/ListMovie.jsx
+++ src/components/ListMovie.jsx
@@ -3,11 +3,15 @@ function ListMovie(props) {
         const { title, genre } = movie
         return (
             <li key={crypto.randomUUID()}>
-                <div className="mb-2">
-                    <span className="badge rounded-pill bg-primary-subtle text-primary text-uppercase">
-                        {genre}
-                    </span>
-                    <h2 className="card-title fw-bold text-dark mb-0">{title}</h2>
+                <div className="col-12 col-md-6 col-lg-4 mb-4">
+                    <div className="card h-100 shadow-sm border-0 bg-white hover-shadow">
+                        <div className="card-body">
+                            <span className="badge rounded-pill bg-primary-subtle text-primary text-uppercase">
+                                {genre}
+                            </span>
+                            <h2 className="card-title fw-bold text-dark mb-0">{title}</h2>
+                        </div>
+                    </div>
                 </div>
             </li >
         )

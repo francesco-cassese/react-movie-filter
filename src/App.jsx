51a37fd29ff6db8b6e7e7ad89dbdc59c065c1d81
Francesco Cassese <francescocassese9@gmail.com>
2026-05-06 18:55:26 +0200

    refactor: spostata la form in un componente singolo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Header from './components/Header.jsx';
 import movieList from './data/movieList.js';
 import ListMovie from './components/ListMovie.jsx';
+import SearchBar from './components/SearchBar.jsx';
 
 function App() {
   const [listMovie, setListMovie] = useState(movieList)
@@ -34,43 +35,13 @@ function App() {
   return (
     <>
       <Header />
-      <main>
-        <form className='p-4 bg-light rounded shadow-sm'>
-          <div className="row g-3">
-            <div className="col-md-6">
-              <label htmlFor="searchMovie" className="form-label fw-bold text-secondary small">
-                Cerca un film
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="searchMovie"
-                placeholder="Inserisci il titolo..."
-                value={search}
-                onChange={changeInputHandler}
-                name='searchMovie'
-              />
-            </div>
+      <main className="container">
+        <SearchBar
+          search={search}
+          selectedGenre={selectedGenre}
+          onInputChange={changeInputHandler}
+        />
 
-            <div className="col-md-6">
-              <label htmlFor="movieGenre" className="form-label fw-bold text-secondary small">
-                Filtra per Genere
-              </label>
-              <select
-                className="form-select"
-                id="movieGenre"
-                value={selectedGenre}
-                onChange={changeInputHandler}
-                name='movieGenre'>
-                <option value="">Tutti i generi</option>
-                <option value="Azione">Azione</option>
-                <option value="Thriller">Thriller</option>
-                <option value="Romantico">Romantico</option>
-                <option value="Fantascienza">Fantascienza</option>
-              </select>
-            </div>
-          </div>
-        </form>
         <div className="container py-2">
           <div className="row g-4">
             <ListMovie listMovie={listFiltered} />
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,42 @@
+function SearchBar(props) {
+    const { search, onInputChange, selectedGenre } = props
+    return (
+        <form className='p-4 bg-light rounded shadow-sm'>
+            <div className="row g-3">
+                <div className="col-md-6">
+                    <label htmlFor="searchMovie" className="form-label fw-bold text-secondary small">
+                        Cerca un film
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="searchMovie"
+                        placeholder="Inserisci il titolo..."
+                        value={search}
+                        onChange={onInputChange}
+                        name='searchMovie'
+                    />
+                </div>
+
+                <div className="col-md-6">
+                    <label htmlFor="movieGenre" className="form-label fw-bold text-secondary small">
+                        Filtra per Genere
+                    </label>
+                    <select
+                        className="form-select"
+                        id="movieGenre"
+                        value={selectedGenre}
+                        onChange={onInputChange}
+                        name='movieGenre'>
+                        <option value="">Tutti i generi</option>
+                        <option value="Azione">Azione</option>
+                        <option value="Thriller">Thriller</option>
+                        <option value="Romantico">Romantico</option>
+                        <option value="Fantascienza">Fantascienza</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+    )
+}
+export default SearchBar

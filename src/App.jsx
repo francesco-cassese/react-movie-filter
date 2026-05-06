88c5ab668d12bb4f8619d21736099caa1b57cbb5
Francesco Cassese <francescocassese9@gmail.com>
2026-05-06 16:17:30 +0200

    feat: aggiunta struttura base degli input per la ricerca e il select

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import { useState, useEffect } from 'react';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
+import movieList from './data/movieList.js';
 
 function App() {
+  const [search, setSearch] = useState(movieList);
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <form>
+          <div>
+            <div className="col-md-6">
+              <label htmlFor="searchMovie" className="form-label fw-bold">
+                Cerca un film
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="search-movie"
+                placeholder="Inserisci il titolo..."
+              />
+            </div>
+
+            <div className="col-md-6">
+              <label htmlFor="movieGenre" className="form-label fw-bold">
+                Genere
+              </label>
+              <select className="form-select" id="movieGenre" defaultValue="">
+                <option value="" disabled>Scegli un genere...</option>
+                <option value="azione">Azione</option>
+                <option value="commedia">Commedia</option>
+                <option value="drammatico">Drammatico</option>
+                <option value="fantascienza">Fantascienza</option>
+              </select>
+            </div>
+          </div>
+        </form>
+      </main>
+    </>
   );
 }
 export default App;

5545d5a8b91e8808ccd1c7d78d57df67984e56b3
Francesco Cassese <francescocassese9@gmail.com>
2026-05-06 15:58:41 +0200

    feat: aggiunto e personalizzato header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <header className="mb-4 bg-dark text-white custom-header">
+      <div className="container-fluid py-3">
+        <div className="row justify-content-center text-center">
+          <div className="col-lg-8">
+            <h1 className="display-1 fw-bold mb-3 tracking-tight">
+              React Movie Filter
+            </h1>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 export default App;

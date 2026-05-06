2816b4b2af3bdb4cb1a7e4d0533ee7fb8f97366d
Francesco Cassese <francescocassese9@gmail.com>
2026-05-06 15:05:27 +0200

    feat: aggiunta lista in movieList.js nella cartella data

---
+++ src/data/movieList.js
@@ -0,0 +1,21 @@
+
+const movieList = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+    { title: 'Matrix', genre: 'Fantascienza' },
+    { title: 'Se7en', genre: 'Thriller' },
+    { title: 'Le Pagine della Nostra Vita', genre: 'Romantico' },
+    { title: 'Mad Max: Fury Road', genre: 'Azione' },
+    { title: 'Blade Runner 2049', genre: 'Fantascienza' },
+    { title: 'Shutter Island', genre: 'Thriller' },
+    { title: 'Io prima di te', genre: 'Romantico' },
+    { title: 'John Wick', genre: 'Azione' },
+    { title: 'Arrival', genre: 'Fantascienza' },
+    { title: 'Il Silenzio degli Innocenti', genre: 'Thriller' },
+];
+
+export default movieList;
